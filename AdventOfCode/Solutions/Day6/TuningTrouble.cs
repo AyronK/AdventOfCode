00a8563a3f160ca33eab538bcffd0ebeed67e740
AyronK <kotlickimarcin@gmail.com>
2022-12-06 09:46:36 +0100

    Day 6 Tuning Trouble - Part 2 and fix for Part 1

--- AdventOfCode/Solutions/Day6/TuningTrouble.cs
+++ AdventOfCode/Solutions/Day6/TuningTrouble.cs
@@ -9,24 +9,33 @@ internal class TuningTrouble : ISolution
         using var file = File.OpenRead(entryPoint.InputPath);
         using var reader = new StreamReader(file);
         var line = reader.ReadLine() ?? string.Empty;
+        
+        Queue<char> buffer = new(14);
 
-        HashSet<char> set = new(4);
-        
-        int i = 0;
-        
-        for (; i < line.Length; i++)
+        int initialSequenceIndex = 0;
+        int messageSequenceIndex = 0;
+
+        for (int i = 0; i < line.Length; i++)
         {
-            if (!set.Add(line[i]))
+            if (buffer.Count == 14)
             {
-                set.Clear();
+                buffer.Dequeue();
             }
             
-            if (set.Count == 4)
+            buffer.Enqueue(line[i]);
+
+            if (initialSequenceIndex == 0 && buffer.TakeLast(4).Distinct().Count() == 4)
             {
-                break;
+                initialSequenceIndex = i + 1;
+            }
+
+            if (messageSequenceIndex == 0 && buffer.TakeLast(14).Distinct().Count() == 14)
+            {
+                messageSequenceIndex = i + 1;
             }
         }
 
-        Console.WriteLine($"The elvish protocol starting sequence comes after {i} characters.");
+        Console.WriteLine($"The elvish protocol starting sequence comes after {initialSequenceIndex} characters.");
+        Console.WriteLine($"The elvish protocol message sequence comes after {messageSequenceIndex} characters.");
     }
 }
