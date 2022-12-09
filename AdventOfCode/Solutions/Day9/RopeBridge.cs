a7769fc7ae6aca52d6259b6107bd89cbe7c9b773
AyronK <kotlickimarcin@gmail.com>
2022-12-09 10:46:23 +0100

    Day 9 Rope Bridge - Part 2

--- AdventOfCode/Solutions/Day9/RopeBridge.cs
+++ AdventOfCode/Solutions/Day9/RopeBridge.cs
@@ -27,28 +27,41 @@ internal class RopeBridge : ISolution
 {
     public void Run(EntryPoint entryPoint)
     {
-        var moves = File.ReadAllLines(entryPoint.InputPath).Select(ParseLine);
+        var moves = File.ReadAllLines(entryPoint.InputPath).Select(ParseLine).ToArray();
 
-        var head = new Point(0, 0);
-        var tail = new Point(0, 0);
+        var shortRope = Enumerable.Repeat(new Point(0, 0), 2).ToArray();
+        var longRope = Enumerable.Repeat(new Point(0, 0), 10).ToArray();
 
-        var visited = new HashSet<Point> { tail };
+        var visitedInShort = FindPositionsVisitedByTail(shortRope, moves);
+        Console.WriteLine($"Tail of the short rope visited {visitedInShort.Count} unique positions.");
+
+        var visitedInLong = FindPositionsVisitedByTail(longRope, moves);
+        Console.WriteLine($"Tail of the long rope visited {visitedInLong.Count} unique positions.");
+    }
+
+    private static HashSet<Point> FindPositionsVisitedByTail(Point[] rope, IEnumerable<Move> moves)
+    {
+        var visited = new HashSet<Point> { rope[1] };
 
         foreach (var move in moves)
         {
             for (int i = 0; i < move.Length; i++)
             {
-                head = MoveInDirection(head, move.Direction);
+                rope[0] = MoveInDirection(rope[0], move.Direction);
 
-                if (!tail.Touch(head))
+                for (int ropeNode = 1; ropeNode < rope.Length; ropeNode++)
                 {
-                    tail = MoveTowards(tail, head);
-                    visited.Add(tail);
+                    if (!rope[ropeNode].Touch(rope[ropeNode - 1]))
+                    {
+                        rope[ropeNode] = MoveTowards(rope[ropeNode], rope[ropeNode - 1]);
+                    }
                 }
+
+                visited.Add(rope[^1]);
             }
         }
 
-        Console.WriteLine($"Tail rope visited {visited.Count} unique positions.");
+        return visited;
     }
 
     private static Point MoveInDirection(Point point, Direction direction)
