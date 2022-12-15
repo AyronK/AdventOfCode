42a2a7d92fc8189aa15bae7b4d28d18bf32af40b
AyronK <kotlickimarcin@gmail.com>
2022-12-15 19:40:05 +0100

    Refactor and optimization

--- AdventOfCode/Solutions/Day15/BeaconExclusionZone.cs
+++ AdventOfCode/Solutions/Day15/BeaconExclusionZone.cs
@@ -4,106 +4,127 @@ namespace AdventOfCode.Cli.Solutions.Day15;
 
 internal class BeaconExclusionZone : ISolution
 {
-    record Point(int X, int Y);
+    private record Point(int X, int Y);
 
-    record Area(Point Sensor, Point Beacon, int Distance);
+    private record Range(Point Start, Point End)
+    {
+        public bool Covers(int x) => Start.X == x || End.X == x || (Start.X < x && End.X > x);
+    };
+
+    private record Area(Point Sensor, Point Beacon, int Radius)
+    {
+        public Range? Covers(int y)
+        {
+            var diameter = 2 * Radius + 1;
+            var yOffset = Math.Abs(y - Sensor.Y);
+
+            if (yOffset > Radius)
+            {
+                return null;
+            }
+
+            var halfChord = (diameter - 2 * yOffset) / 2;
+            return new Range(new Point(Sensor.X - halfChord, y), new Point(Sensor.X + halfChord, y));
+        }
+    };
 
     public void Run(EntryPoint entryPoint)
     {
-        var input = File.ReadAllLines(entryPoint.InputPath).Select(l =>
-        {
-            l = new string(l.SkipWhile(c => c != '=').ToArray());
-            var x = int.Parse(l[1..l.IndexOf(',')]);
-            l = new string(l.Skip(l.IndexOf(',')).SkipWhile(c => c != '=').ToArray());
-            var y = int.Parse(l[1..l.IndexOf(':')]);
-            l = new string(l.Skip(l.IndexOf(':')).SkipWhile(c => c != '=').ToArray());
-            var x2 = int.Parse(l[1..l.IndexOf(',')]);
-            l = new string(l.Skip(l.IndexOf(',')).SkipWhile(c => c != '=').ToArray());
-            var y2 = int.Parse(l[1..]);
+        var mapOfSignal = File.ReadAllLines(entryPoint.InputPath).Select(ParseInput).OrderBy(c => c.Sensor.X).ToArray();
 
-            var sensor = new Point(x, y);
-            var beacon = new Point(x2, y2);
-            var distance = ManhattanDistance(sensor, beacon);
-            return new Area(sensor, beacon, distance);
-        }).OrderBy(c => c.Sensor.X).ToArray();
+        int rowToCheck = 2_000_000;
+        int searchThreshold = 4_000_000;
+        
+        var blockedInRowToCheck = CountBlocked(mapOfSignal, rowToCheck);
+        var tuningFrequency = FindTuningFrequency(mapOfSignal, searchThreshold);
 
-        int rowToCheck = 2000000;
-        int threshold = 4000000;
-
-        int xMin = input.Min(c => c.Sensor.X - c.Distance);
-        int xMax = input.Max(c => c.Sensor.X + c.Distance);
-
-        int count = 0;
-
-        HashSet<Point> beacons = new(input.Select(c => c.Beacon));
-        HashSet<Point> possibleBeacons = new();
-
-        var searchYMin = Math.Min(rowToCheck, 0);
-        var searchYMax = Math.Max(rowToCheck, threshold);
-
-        for (int y = searchYMin; y <= searchYMax; y++)
-        {
-            var searchMinX = y == rowToCheck ? xMin : 0;
-            var searchMaxX = y == rowToCheck ? xMax : threshold;
-
-            for (int x = searchMinX; x <= searchMaxX; x++)
-            {
-                var currentPoint = new Point(x, y);
-                possibleBeacons.Add(currentPoint);
-
-                if (!beacons.Contains(currentPoint) &&
-                    input.FirstOrDefault(e => e.Distance >= ManhattanDistance(e.Sensor, currentPoint)) is { } found)
-                {
-                    if (y == rowToCheck)
-                    {
-                        count++;
-                    }
-                    else
-                    {
-                        var fullDiameter = (2 * found.Distance + 1);
-                        var yOffset = Math.Abs(currentPoint.Y - found.Sensor.Y);
-                        var amountOfBlockedInRow = fullDiameter - 2 * yOffset;
-
-                        x += (amountOfBlockedInRow / 2) - Math.Abs(currentPoint.X - found.Sensor.X);
-                    }
-
-                    possibleBeacons.Remove(currentPoint);
-                }
-            }
-        }
-
-        foreach (var possibleBeacon in possibleBeacons)
-        {
-            if (!beacons.Contains(possibleBeacon) && IsValidDistressBeacon(possibleBeacon, threshold))
-            {
-                Console.WriteLine($"{possibleBeacon.X},{possibleBeacon.Y}");
-                Console.WriteLine(GetTuningFrequency(possibleBeacon));
-            }
-        }
-
-        Console.WriteLine(count);
+        Console.WriteLine($"In row {rowToCheck} there are {blockedInRowToCheck} positions cannot contain a beacon.");
+        Console.WriteLine($"Tuning frequency for distressed beacon is equal to {tuningFrequency}.");
     }
 
-    static long GetTuningFrequency(Point a)
+    private Area ParseInput(string l)
     {
-        checked
-        {
-            return (long)a.X * 4000000 + a.Y;
-        }
+        var values = l.Split(':', '=', ',').Where(s => int.TryParse(s, out _)).Select(int.Parse).ToArray();
+        var sensor = new Point(values[0], values[1]);
+        var beacon = new Point(values[2], values[3]);
+        var radius = ManhattanDistance(new Point(values[0], values[1]), new Point(values[2], values[3]));
+        return new Area(sensor, beacon, radius);
     }
 
-    static bool IsValidDistressBeacon(Point a, int threshold)
+    private static int CountBlocked(Area[] areas, int rowToCheck)
+    {
+        var blockedInRowToCheck = 0;
+
+        HashSet<Point> beacons = new(areas.Select(c => c.Beacon));
+        var xMin = areas.Min(c => c.Sensor.X - c.Radius);
+        var xMax = areas.Max(c => c.Sensor.X + c.Radius);
+
+        var rangesForRowToCheck = GetCoverRanges(areas, rowToCheck);
+
+        for (var x = xMin; x <= xMax; x++)
+        {
+            if (!beacons.Contains(new Point(x, rowToCheck)) && rangesForRowToCheck.Any(r => r.Covers(x)))
+            {
+                blockedInRowToCheck++;
+            }
+        }
+
+        return blockedInRowToCheck;
+    }
+
+    private static long FindTuningFrequency(Area[] areas, int searchThreshold)
+    {
+        Point? result = null;
+        Parallel.For(0, searchThreshold, (y, ctx) =>
+        {
+            var ranges = GetCoverRanges(areas, y);
+            var prev = 0;
+
+            foreach (var (start, end) in ranges)
+            {
+                if (start.X > prev + 1 && end.X > prev + 1)
+                {
+                    var point = new Point(prev + 1, y);
+
+                    if (IsValidDistressBeacon(point, searchThreshold))
+                    {
+                        result = point;
+                        ctx.Break();
+                    }
+                }
+
+                prev = Math.Max(prev, end.X);
+            }
+        });
+
+        if (result is null)
+        {
+            throw new InvalidOperationException("Cannot find tuning frequency for given input");
+        }
+
+        return GetTuningFrequency(result);
+    }
+
+    private static Range[] GetCoverRanges(Area[] input, int rowToCheck)
+    {
+        IEnumerable<Range> AccumulateRanges(IEnumerable<Range> prev, Area curr) => curr.Covers(rowToCheck) is { } r ? prev.Append(r) : prev;
+        return input.Aggregate(Enumerable.Empty<Range>(), AccumulateRanges).OrderBy(c => c.Start.X).ToArray();
+    }
+
+    private static long GetTuningFrequency(Point a) => checked((long)a.X * 4000000 + a.Y);
+
+    private static bool IsValidDistressBeacon(Point a, int threshold)
     {
         return a.X >= 0 && a.X <= threshold && a.Y >= 0 && a.Y <= threshold;
     }
 
-    static int ManhattanDistance(Point a, Point b)
+    private static int ManhattanDistance(Point a, Point b)
     {
         var sub = Subtract(a, b);
         return Math.Abs(sub.X) + Math.Abs(sub.Y);
     }
 
-    static Point Subtract(Point a, Point b)
+    private static Point Subtract(Point a, Point b)
     {
         return new Point(a.X - b.X, a.Y - b.Y);
     }
